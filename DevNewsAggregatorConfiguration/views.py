ba8460ebb09dbc5f8fd593ed70e339a82c6d505f
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-11-12 22:38:53 -0500

    Allow user to register for an account

--- DevNewsAggregatorConfiguration/views.py
+++ DevNewsAggregatorConfiguration/views.py
@@ -1,3 +1,22 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            # TODO: Redirect properly once servers are integrated
+            return HttpResponseRedirect("http://127.0.0.1/DevNewsAggregator/index.php")
+    else:
+        form = UserCreationForm()
+    return render(request, "DevNewsAggregatorConfiguration/register.html", {
+        'form': form,
+    })
--- PyDevNewsAggregator/settings.py
+++ PyDevNewsAggregator/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'DevNewsAggregatorConfiguration',
+    'DevNewsAggregatorConfiguration',
 )
 
 MIDDLEWARE_CLASSES = (
