c8cd37fc970d03f3fcf06a1700f45944754b40e2
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-11-12 20:16:38 -0500

    Created the HtmlContent model

--- PyDevNewsAggregator/DevNewsAggregatorConfiguration/models.py
+++ PyDevNewsAggregator/DevNewsAggregatorConfiguration/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class HtmlContent(models.Model):
+    url = models.TextField()
+    name = models.CharField(max_length=255)
+    scraping_strategy = models.IntegerField()
+    outer_content_selector = models.TextField()
+    inner_content_selector = models.TextField()
+    title_selector = models.TextField()
+    ignore_first_n_posts = models.IntegerField(default=0)
+    ignore_last_n_posts = models.IntegerField(default=0)
+    date_parsing_strategy = models.IntegerField()
+    date_selector = models.TextField()
+    time_selector = models.TextField()
+    
+
