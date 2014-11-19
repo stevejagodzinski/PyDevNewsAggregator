1fdeebdcd03a0b76b71886266c03b3007b89b59f
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-11-19 03:59:46 -0500

    Preserve username and e-mail on invalid registration

--- DevNewsAggregatorConfiguration/views.py
+++ DevNewsAggregatorConfiguration/views.py
@@ -19,10 +19,12 @@ def register(request):
         try:
             User.objects.create_user(username, email, password);
         except IntegrityError:
-            return HttpResponseRedirect("/DevNewsAggregatorConfiguration/register/?username_error=duplicate");
+            return HttpResponseRedirect("/DevNewsAggregatorConfiguration/register/?username_error=duplicate&username=" + username + "&email=" + email);
         return HttpResponseRedirect("http://127.0.0.1/DevNewsAggregator/index.php")
     else:
         return render(request, "DevNewsAggregatorConfiguration/register.html", {
+            'email': request.GET.get('email'),
+            'username': request.GET.get('username'),
             'username_error': __get_error_message_for_username_field(request.GET.get('username_error'))
         })
 
