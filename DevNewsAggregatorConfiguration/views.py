f058a1f9b57fd816875109a8ec43d316d956eb31
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-11-19 03:41:04 -0500

    Show error when username already exists

--- DevNewsAggregatorConfiguration/views.py
+++ DevNewsAggregatorConfiguration/views.py
@@ -16,11 +16,16 @@ def register(request):
         password = request.POST['password']
         email = request.POST['email']
 
-        # TODO: Catch unique constraint violation and report on the UI
         try:
             User.objects.create_user(username, email, password);
         except IntegrityError:
-            return HttpResponseRedirect("/DevNewsAggregatorConfiguration/register/");
+            return HttpResponseRedirect("/DevNewsAggregatorConfiguration/register/?username_error=duplicate");
         return HttpResponseRedirect("http://127.0.0.1/DevNewsAggregator/index.php")
     else:
-        return render(request, "DevNewsAggregatorConfiguration/register.html")
+        return render(request, "DevNewsAggregatorConfiguration/register.html", {
+            'username_error': __get_error_message_for_username_field(request.GET.get('username_error'))
+        })
+
+def __get_error_message_for_username_field(username_error) :
+    if username_error == 'duplicate':
+        return "This username has already been chosen."
