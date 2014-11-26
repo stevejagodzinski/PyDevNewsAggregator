fc823c8945c30f0c4294736a202bd1285057a661
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-11-25 23:38:02 -0500

    Render content body in metronic template

--- DevNewsAggregatorConfiguration/urls.py
+++ DevNewsAggregatorConfiguration/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from DevNewsAggregatorConfiguration import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.feed, name='feed'),
     url(r'^login/', views.login, name='login'),
     url(r'^register/', views.register, name='register'),
 )
