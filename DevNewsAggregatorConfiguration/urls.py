8206ef1f30689f41420c99ddc9b56757ff2df645
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-11-28 23:37:25 -0500

    Ability to update existing content source

--- DevNewsAggregatorConfiguration/urls.py
+++ DevNewsAggregatorConfiguration/urls.py
@@ -3,6 +3,7 @@ from DevNewsAggregatorConfiguration.views import feed, login, add_edit
 
 urlpatterns = patterns('',
                        url(r'^$', feed.feed, name='feed'),
+                       url(r'^html_content/(\d+)/', add_edit.get_or_update_html_content, name='get_or_update_html_content'),
                        url(r'^html_content/new/', add_edit.new_html_content, name='new_html_content'),
                        url(r'^login/', login.app_login, name='login'),
                        url(r'^logout/', login.app_logout, name='logout'),
--- DevNewsAggregatorConfiguration/views/add_edit.py
+++ DevNewsAggregatorConfiguration/views/add_edit.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from DevNewsAggregatorConfiguration.models import ScrapingStrategy, HtmlContentForm
+from django.shortcuts import render, get_object_or_404
+from DevNewsAggregatorConfiguration.models import ScrapingStrategy, HtmlContentForm, HtmlContent
 from DevNewsAggregatorConfiguration.views import view_utils
 
 
@@ -10,10 +10,21 @@ def new_html_content(request):
             form.save()
     else:
         form = HtmlContentForm()
+    return __render_add_edit_form(request, form)
 
+
+def get_or_update_html_content(request, html_content_id):
+    instance = get_object_or_404(HtmlContent, id=html_content_id)
+    form = HtmlContentForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        form.save()
+    return __render_add_edit_form(request, form)
+
+
+def __render_add_edit_form(request, form):
     return render(request, "DevNewsAggregatorConfiguration/add_edit.html", {
-            'authenticated': request.user.is_authenticated(),
-            'form': form,
-            'scraping_strategies': ScrapingStrategy.get_all_sorted_by_display_string(),
-            'username': view_utils.get_username_for_top_nav_menu(request)
-        })
+        'authenticated': request.user.is_authenticated(),
+        'form': form,
+        'scraping_strategies': ScrapingStrategy.get_all_sorted_by_display_string(),
+        'username': view_utils.get_username_for_top_nav_menu(request)
+    })
