58448516937250b4400fdc1acf5dffc983c037c6
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-11-28 23:50:37 -0500

    Redirect on a successful save to avoid form submission

--- DevNewsAggregatorConfiguration/views/add_edit.py
+++ DevNewsAggregatorConfiguration/views/add_edit.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from DevNewsAggregatorConfiguration.models import ScrapingStrategy, HtmlContentForm, HtmlContent
 from DevNewsAggregatorConfiguration.views import view_utils
@@ -7,7 +8,8 @@ def new_html_content(request):
     if request.method == 'POST':
         form = HtmlContentForm(request.POST)
         if form.is_valid():
-            form.save()
+            instance = form.save()
+            return __redirect_after_successful_save(instance)
     else:
         form = HtmlContentForm()
     return __render_add_edit_form(request, form)
@@ -16,9 +18,14 @@ def new_html_content(request):
 def get_or_update_html_content(request, html_content_id):
     instance = get_object_or_404(HtmlContent, id=html_content_id)
     form = HtmlContentForm(request.POST or None, instance=instance)
-    if form.is_valid():
-        form.save()
+    if request.method == 'POST' and form.is_valid():
+        instance = form.save()
+        return __redirect_after_successful_save(instance)
     return __render_add_edit_form(request, form)
+
+
+def __redirect_after_successful_save(html_content_instance):
+    return HttpResponseRedirect("DevNewsAggregatorConfiguration/html_content/%d/" % html_content_instance.id)
 
 
 def __render_add_edit_form(request, form):
