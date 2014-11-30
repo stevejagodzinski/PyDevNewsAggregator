f3c7ca6ac9e5ca7a190eef968c4d24b249fa09a1
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-11-30 03:10:31 -0500

    Send back empty HttpResponse when user unsubscribes from content

--- DevNewsAggregatorConfiguration/views/html_content_user.py
+++ DevNewsAggregatorConfiguration/views/html_content_user.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse
 from DevNewsAggregatorConfiguration.models import HtmlContent
 import logging
 
@@ -13,3 +14,4 @@ def unsubscribe_user_from_html_content(request, html_content_name):
             html_content.save()
         except HtmlContent.DoesNotExist:
             logger.warn("Can not unsubscribe user from content source [%s] because it no longer exists." % html_content_name)
+    return HttpResponse();
