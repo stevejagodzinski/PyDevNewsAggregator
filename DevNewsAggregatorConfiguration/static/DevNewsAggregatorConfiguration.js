67bbaf5c8aeecc1449c571abdbbf9a5413a6a89d
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-11-30 15:18:25 -0500

    Link to add/edit pages in quick sidebar

--- DevNewsAggregatorConfiguration/static/DevNewsAggregatorConfiguration.js
+++ DevNewsAggregatorConfiguration/static/DevNewsAggregatorConfiguration.js
@@ -9,3 +9,29 @@ $('.make-switch').on('switchChange.bootstrapSwitch', function (event, state) {
         });
     }
 });
+
+var quickSidebarItems = $('.page-quick-sidebar-wrapper .page-quick-sidebar .list-items > li');
+quickSidebarItems.click(function (e) {
+    var target = $(e.target);
+    var mousePosition = e.offsetX || e.clientX - target.offset().left;
+    if (mousePosition >= 10 && mousePosition <= 27) {
+        quickSidebarIconClicked(target);
+    }
+});
+
+function quickSidebarIconClicked(target) {
+    if(target.attr('id') == 'add-new-content-source') {
+        handleAddClicked();
+    } else {
+        handleEditClicked(target);
+    }
+}
+
+function handleAddClicked() {
+    window.location.replace("/DevNewsAggregatorConfiguration/html_content/new/");
+}
+
+function handleEditClicked(target) {
+    var htmlContentId = target.attr('data-news-source-id');
+    window.location.replace("/DevNewsAggregatorConfiguration/html_content/" + htmlContentId + "/");
+}
