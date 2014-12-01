c168ba898ca5176d6b418d25304fdbcdb9907a7f
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-11-30 19:31:15 -0500

    Add news entry ajax response to dom

--- DevNewsAggregatorConfiguration/static/DevNewsAggregatorConfiguration.js
+++ DevNewsAggregatorConfiguration/static/DevNewsAggregatorConfiguration.js
@@ -3,7 +3,8 @@ $('.make-switch').on('switchChange.bootstrapSwitch', function (event, state) {
     if(state) {
         $.ajax("/DevNewsAggregatorConfiguration/html_content_user/add/"+ dataNewsSourceName + "/", {
             headers: {'X-CSRFToken': $.cookie('csrftoken')},
-            type: "POST"
+            type: "POST",
+            success: handleContentAdded
         });
     } else {
         $("[data-content-source='" + dataNewsSourceName + "']").remove();
@@ -43,4 +44,35 @@ function handleAddClicked() {
 function handleEditClicked(target) {
     var htmlContentId = target.attr('data-news-source-id');
     window.location.href = "/DevNewsAggregatorConfiguration/html_content/" + htmlContentId + "/";
+}
+
+function handleContentAdded(content) {
+    var contentContainer = $('.content');
+    var existingNewsEntries = contentContainer.children('.news-entry');
+    var newNewsEntries = $(content).children('.news-entry');
+
+    var newNewsEntriesTraversalIndex = 0;
+
+    for (var existingNewsEntriesTraversalIndex = 0; existingNewsEntriesTraversalIndex<existingNewsEntries.length && newNewsEntriesTraversalIndex<newNewsEntries.length; existingNewsEntriesTraversalIndex++) {
+        var existingNewsEntry = existingNewsEntries.eq(existingNewsEntriesTraversalIndex);
+
+        // TODO: Set timezone on php server
+        var existingNewsEntryDate = new Date(existingNewsEntry.find('.news-entry-date').attr('data-iso-date'));
+
+        do{
+            var inserted = false;
+            // TODO: Don't run this jquery every time
+            var newNewsEntry = newNewsEntries.eq(newNewsEntriesTraversalIndex);
+            var newNewsEntryDate = new Date(newNewsEntry.find('.news-entry-date').attr('data-iso-date'));
+            if(newNewsEntryDate > existingNewsEntryDate) {
+                newNewsEntry.insertBefore(existingNewsEntry);
+                inserted = true;
+                newNewsEntriesTraversalIndex++;
+            }
+        }while(inserted);
+    }
+
+    for(var i=newNewsEntriesTraversalIndex; i<newNewsEntries.length; i++) {
+        contentContainer.append(newNewsEntries.get(i));
+    }
 }
